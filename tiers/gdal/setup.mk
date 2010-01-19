e59d632ec3946f8e6aad7dcbe7a58890635ea37b
david <devnull@localhost>
2010-01-19 17:29:18 +0000

    Fixed patch command

--- tiers/gdal/setup.mk
+++ tiers/gdal/setup.mk
@@ -29,4 +29,4 @@ co:
 	fi
 
 apply_patches:
-	cd $(DEPTH)/../gdal;$(foreach p,$(PATCH_FILES),patch < "$(_curdir)/patches/$(p)";) echo
+	cd $(DEPTH)/../gdal;$(foreach p,$(PATCH_FILES),patch -p0 < "$(_curdir)/patches/$(p)";) echo
